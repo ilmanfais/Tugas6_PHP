40b4f4c853d38f326f947d437f001ada5558a29b
M. FASRUL FAIS ILMAN <ilmanfaisocu26@gmail.com>
2023-12-12 07:49:57 +0700

    up

--- loginform.php
+++ loginform.php
@@ -33,57 +33,74 @@
         }
     }
     ?>
-
-    <div class="container mt-5">
-        <nav class="navbar navbar-expand-lg navbar-light bg-gray-200">
+        <nav class="navbar bg-body-tertiary">
             <div class="container-fluid">
-                <button class="navbar-brand bg-primary border-info text-white" href="#">Toggle Menu</button>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="btn btn-primary mt-3 mb-3  text-white border-3 border-info"  href="#">Toogle Menu</button>
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item dropdown">
-                            <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </button>
-                            <ul class="dropdown-menu dropdown-menu-dark">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form class="d-flex mt-3" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </nav>
-
-        <h2 class="mb-4">Form Login</h2>
-        <form action="" method="post">
-            <div class="mb-3 row">
-                <label for="username" class="col-sm-2 col-form-label">Username:</label>
-                <div class="col-sm-10">
-                    <input type="text" id="username" name="username" class="form-control" required>
+        <div class="mx-3">
+            <h2 class="mb-4">Form Login</h2>
+            <form action="" method="post">
+                <div class="mb-3 row">
+                    <label for="username" class="col-sm-2 col-form-label">Username:</label>
+                    <div class="col-sm-10">
+                        <input type="text" id="username" name="username" class="form-control" required>
+                    </div>
+    
+                    <label for="password" class="col-sm-2 col-form-label mt-3">Password:</label>
+                    <div class="col-sm-10">
+                        <input type="password" id="password" name="password" class="form-control mt-3" required>
+                    </div>
+    
+                    <div class="col-sm-10 offset-sm-2">
+                        <button type="submit" class="btn btn-primary mt-3 mb-3">Login</button>
+                        <?php
+                        if (isset($pesan)) {
+                            echo "<p>$pesan</p>";
+                        }
+                        ?>
+                    </div>
+    
+    
                 </div>
-
-                <label for="password" class="col-sm-2 col-form-label mt-3">Password:</label>
-                <div class="col-sm-10">
-                    <input type="password" id="password" name="password" class="form-control mt-3" required>
-                </div>
-
-                <div class="col-sm-10 offset-sm-2">
-                    <button type="submit" class="btn btn-primary mt-3 mb-3">Login</button>
-                    <?php
-                    if (isset($pesan)) {
-                        echo "<p>$pesan</p>";
-                    }
-                    ?>
-                </div>
-
-
-            </div>
-        </form>
-    </div>
+            </form>
+        </div>
 </body>
 
 </html>
